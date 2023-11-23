f91a34b6c532b91afd1d1ec0a9c62c0be897caa7
y0n1 <y0n1@users.noreply.github.com>
2023-11-23 14:43:44 +0200

    Update and rename popcat-autoclicker.js to popcatClicker.js

---
+++ popcatClicker.js
@@ -0,0 +1,21 @@
+let interval = null;
+const doClick = () => () => {
+  document.dispatchEvent(new PointerEvent('pointerdown'));
+  document.dispatchEvent(new PointerEvent('pointerup'));
+};
+const start = (delay = 250) => {
+  if (interval === null) {
+    interval = setInterval(doClick, delay);
+  }
+};
+const stop = () => {
+  if (interval !== null) {
+    clearInterval(interval);
+    interval = null;
+  }
+};
+
+export const popcatClicker = {
+  start,
+  stop,
+};
